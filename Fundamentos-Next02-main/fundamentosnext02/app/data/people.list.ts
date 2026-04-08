7d28b14e0b81509824e9b66fb5f7e63d90c98255
Andriele <andriele.p.martins@aluno.senai.br>
2026-04-08 14:07:58 -0300

    Add files via upload
    08/04

---
+++ Fundamentos-Next02-main/fundamentosnext02/app/data/people.list.ts
@@ -0,0 +1,8 @@
+import { person } from "../types/people";
+
+export const peopleList: person[] = [
+    {id: 1, name: "Rafael", profession: "Programador"},
+    {id: 2, name: "Vini", profession: "Programador"},
+    {id: 3, name: "Andrieli", profession: "Programadora"},
+    {id: 4, name: "Cesar", profession: "Programador"},
+]
